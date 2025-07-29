cbb83b5c7b191358a55d86ea9d8a97f2a4d4f107
Modibo Keita <modibokeita9390@gmail.com>
2025-07-29 11:48:53 +0300

    updating Die component

--- tenzies/src/Die.jsx
+++ tenzies/src/Die.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
 import "./index.css"
 export default function Die(props) {
+
+      const styles = {
+    backgroundColor: props.isWrong
+      ? "#ff4d4d" // Red if wrong
+      : props.isHeld
+      ? "#59E391" // Green if held
+      : "white"
+  }
   return (
-    <button onClick={props.handleClick} className='dice-btn'>{props.value}</button>
+    <button
+    style={styles}
+    onClick={props.handleClick} 
+    className='dice-btn'
+
+    >{props.value}</button>
   )
 }
